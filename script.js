8f3b3193c99b564b32e0d4309b2ef78532398103
Z <zacharywhitbread@gmail.com>
2026-09-17 11:09:16 +0100

    Physics & movement!
    Added the ability for the player to jump, and the obstacles to spawn regularly and move across the screen.
    Collision not yet implemented.

--- script.js
+++ script.js
@@ -8,6 +8,41 @@ let gameRunning = false;
 let lastFrameTime = 0;
 let score = 0;
 
+const GRAVITY = 300;
+const PLAYER_STATE = {
+    yPosition: 0,
+    yVelocity: 0,
+    jumping: false,
+    jumpVelocity: 250,
+};
+const OBSTACLES = {
+    width: 40,
+    inactivePosition: 0,
+    maxSpawns: 6,
+    spawnRate: 2000,
+    lastSpawnTime: 0,
+    speed: 200,
+    instances: [],
+};
+
+
+function setupObstacles() {
+    OBSTACLES.inactivePosition = obstacleContainer.clientWidth + OBSTACLES.width;
+
+    for (let i=0; i < OBSTACLES.maxSpawns; i++) {
+        const obstacle_element = document.createElement('div');
+        obstacle_element.classList.add("game__obstacle");
+        
+        const new_instance = {
+            xPosition: 0,  // r to l
+            element: obstacle_element,
+            active: false,
+        };
+
+        OBSTACLES.instances.push(new_instance);
+    }
+}
+
 function startGame() {
     if (gameRunning) {
         return;
@@ -19,6 +54,7 @@ function startGame() {
     scoreDisplay.textContent = score;
     message.textContent = "";
     startButton.disabled = true;
+    setupObstacles();
     requestAnimationFrame(gameLoop);
 }
 
@@ -41,14 +77,60 @@ function gameLoop(currentTime) {
 
 function updatePlayer(deltaTime) {
     // Jumping and gravity
+    PLAYER_STATE.yVelocity -= GRAVITY * deltaTime;
+    PLAYER_STATE.yPosition += PLAYER_STATE.yVelocity * deltaTime;
+
+    if (PLAYER_STATE.yPosition <= 0) {
+        PLAYER_STATE.yPosition = 0;
+        PLAYER_STATE.yVelocity = 0;
+        PLAYER_STATE.jumping = false;
+    }
+
+    player.style.bottom = `${PLAYER_STATE.yPosition}px`;
 }
 
 function jump() {
     // The player's jump start
+    if (PLAYER_STATE.jumping) return;
+
+    PLAYER_STATE.jumping = true;
+    PLAYER_STATE.yVelocity = PLAYER_STATE.jumpVelocity;
+}
+
+function spawnObstacle() {
+    const nextObstacle = OBSTACLES.instances.filter(obj => !obj.active)[0];
+
+    if (nextObstacle) {
+        nextObstacle.xPosition = -OBSTACLES.width;
+        nextObstacle.element.style.right = `-${OBSTACLES.width}px`;
+        nextObstacle.active = true;
+        obstacleContainer.appendChild(nextObstacle.element);
+    }
 }
 
 function updateObstacles(deltaTime) {
     // Obstacle generation and movement
+    const currentTime = performance.now();
+
+    // Spawning - time since last
+    if (currentTime - OBSTACLES.lastSpawnTime > OBSTACLES.spawnRate) {
+        OBSTACLES.lastSpawnTime = currentTime;
+
+        spawnObstacle();
+    }
+
+    // Movement
+    OBSTACLES.instances.forEach((obstacle) => {
+        // Set positions
+        if (obstacle.active) {
+            obstacle.xPosition += OBSTACLES.speed * deltaTime;
+            obstacle.element.style.right = `${obstacle.xPosition}px`;
+    
+            if (obstacle.xPosition >= OBSTACLES.inactivePosition) {
+                obstacle.active = false;
+            }
+        }
+    });
 }
 
 function checkCollisions() {
